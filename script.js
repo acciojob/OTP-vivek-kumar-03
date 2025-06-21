ae2722297b0108595ce2bbfe200be80ad3b94461
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-21 21:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    if (value.length > 1) {
+      e.target.value = value.charAt(0);
+    }
+
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = "";
+        }
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
