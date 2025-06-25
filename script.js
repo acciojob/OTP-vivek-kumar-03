67f4a2944f1a155f59723bb0f3d836032187da22
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-25 23:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
-  input.addEventListener('input', () => {
-    const nextInput = inputs[index + 1];
-    if (input.value && nextInput) {
-      nextInput.focus();
+  input.addEventListener('input', (e) => {
+    input.value = input.value.replace(/[^0-9]/g, ''); // Allow only digits
+    if (input.value.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   });
 
   input.addEventListener('keydown', (e) => {
-    const prevInput = inputs[index - 1];
     if (e.key === 'Backspace') {
       if (input.value === '') {
-        if (prevInput) {
-          prevInput.focus();
-          prevInput.value = '';
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = '';
         }
       } else {
         input.value = '';
       }
+    }
+  });
+
+  input.addEventListener('paste', (e) => {
+    e.preventDefault();
+    const paste = e.clipboardData.getData('text').replace(/\D/g, '').slice(0, 6);
+    paste.split('').forEach((digit, i) => {
+      if (inputs[i]) {
+        inputs[i].value = digit;
+      }
+    });
+    if (inputs[paste.length - 1]) {
+      inputs[paste.length - 1].focus();
     }
   });
 });
