b7a0d5b35f27d8386adc8f8951ad6ea3118980a1
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-25 23:25:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-const inputs = document.querySelectorAll(".code");
+const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
-  input.addEventListener("input", (e) => {
-    const value = e.target.value;
-
-    if (value.length > 1) {
-      e.target.value = value.charAt(0);
-    }
-
-    if (value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
+  input.addEventListener('input', () => {
+    const nextInput = inputs[index + 1];
+    if (input.value && nextInput) {
+      nextInput.focus();
     }
   });
 
-  input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace") {
-      if (input.value === "") {
-        if (index > 0) {
-          inputs[index - 1].focus();
-          inputs[index - 1].value = "";
+  input.addEventListener('keydown', (e) => {
+    const prevInput = inputs[index - 1];
+    if (e.key === 'Backspace') {
+      if (input.value === '') {
+        if (prevInput) {
+          prevInput.focus();
+          prevInput.value = '';
         }
       } else {
-        input.value = "";
+        input.value = '';
       }
     }
   });
